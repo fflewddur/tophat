0c4c8846db9d705602a1ed664bc53585fe7472fe
Todd Kulesza <todd@dropline.net>
2020-05-16 09:35:07 -0700

    show CPU meters for each core

--- extension.js
+++ extension.js
@@ -21,8 +21,6 @@
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
-// const Pango = imports.gi.Pango;
-// const PangoCairo = imports.gi.PangoCairo;
 const GTop = imports.gi.GTop;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
@@ -46,6 +44,9 @@ const SECOND_AS_MICROSECONDS = 1000000;
 const METER_BG_COLOR = '#222';
 const METER_FG_COLOR = '#1dacd6';
 
+const CPU_SHOW_CORES = true;
+const CPU_BAR_WIDTH = 10; // width of bars for each CPU core, in pixels
+
 var TopHatCpuIndicator = class TopHatCpuIndicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, `${Me.metadata.name} CPU Indicator`, false);
@@ -57,7 +58,10 @@ var TopHatCpuIndicator = class TopHatCpuIndicator extends PanelMenu.Button {
         let icon = new St.Icon({ gicon, icon_size: 24 });
         hbox.add_child(icon);
 
-        // Exploration of vertical labels
+        // Exploration of vertical labels (but not an efficient way to do this)
+        //
+        // const Pango = imports.gi.Pango;
+        // const PangoCairo = imports.gi.PangoCairo;
         // this.label = new St.DrawingArea({ style_class: 'label' });
         // hbox.add_child(this.label);
         // this.label.connect('repaint', () => {
@@ -67,37 +71,44 @@ var TopHatCpuIndicator = class TopHatCpuIndicator extends PanelMenu.Button {
         //     // let [width, height] = this.meter.get_surface_size();
         //     var _, fg;
         //     [_, fg] = Clutter.Color.from_string('#eee');
-
         //     Clutter.cairo_set_source_color(ctx, fg);
         //     let layout = PangoCairo.create_layout(ctx);
         //     layout.set_font_description(font);
         //     layout.set_line_spacing(0.8);
         //     layout.set_text('C\rP\rU', 5);
         //     PangoCairo.show_layout(ctx, layout);
-
         // });
         // this.label.queue_repaint();
 
+        // Initialize libgtop values
+        this.cpuCores = GTop.glibtop_get_sysinfo().ncpu;
+        this.cpu = new GTop.glibtop_cpu();
+        GTop.glibtop_get_cpu(this.cpu);
+        this.cpuPrev = {
+            user: this.cpu.user,
+            sys: this.cpu.sys,
+            nice: this.cpu.nice,
+            total: this.cpu.total,
+            xcpu_user: new Array(this.cpuCores),
+            xcpu_sys: new Array(this.cpuCores),
+            xcpu_nice: new Array(this.cpuCores),
+            xcpu_total: new Array(this.cpuCores),
+        };
+
+        this.cpuUsage = 0;
+        this.cpuCoreUsage = new Array(this.cpuCores);
+        this.refreshTimer = GLib.timeout_add(GLib.PRIORITY_DEFAULT, UPDATE_INTERVAL_CPU, () => this.refresh());
+
+        // Meter
         this.meter = new St.DrawingArea({ style_class: 'meter' });
+        // Leave 1px of padding between each bar
+        let meterWidth = CPU_BAR_WIDTH * this.cpuCores + this.cpuCores - 1;
+        this.meter.style = `width: ${meterWidth}px;`;
         hbox.add_child(this.meter);
         this.meter.connect('repaint', () => this.repaint());
 
         this.valueCPU = new St.Label({ text: '0%', style_class: 'value' });
         hbox.add_child(this.valueCPU);
-
-        // Initialize libgtop values
-        this.cpu = new GTop.glibtop_cpu();
-        this.cpuCores = GTop.glibtop_get_sysinfo().ncpu;
-        GTop.glibtop_get_cpu(this.cpu);
-        this.cpuPrev = {
-            'user': this.cpu.user,
-            'sys': this.cpu.sys,
-            'nice': this.cpu.nice,
-            'total': this.cpu.total,
-        };
-        this.cpuUsage = 0;
-
-        this.refreshTimer = GLib.timeout_add(GLib.PRIORITY_DEFAULT, UPDATE_INTERVAL_CPU, () => this.refresh());
 
         // Menu
         hbox = new St.BoxLayout();
@@ -114,16 +125,37 @@ var TopHatCpuIndicator = class TopHatCpuIndicator extends PanelMenu.Button {
 
     refresh() {
         GTop.glibtop_get_cpu(this.cpu);
+
+        // Total CPU usage
         let userDelta = this.cpu.user - this.cpuPrev.user;
         let sysDelta = this.cpu.sys - this.cpuPrev.sys;
         let niceDelta = this.cpu.nice - this.cpuPrev.nice;
         let totalDelta = this.cpu.total - this.cpuPrev.total;
         this.cpuUsage = Math.round(100 * (userDelta + sysDelta + niceDelta) / totalDelta);
+
+        // Per-core CPU usage
+        for (let i = 0; i < this.cpuCores; i++) {
+            userDelta = this.cpu.xcpu_user[i] - this.cpuPrev.xcpu_user[i];
+            sysDelta = this.cpu.xcpu_sys[i] - this.cpuPrev.xcpu_sys[i];
+            niceDelta = this.cpu.xcpu_nice[i] - this.cpuPrev.xcpu_nice[i];
+            totalDelta = this.cpu.xcpu_total[i] - this.cpuPrev.xcpu_total[i];
+            this.cpuCoreUsage[i] = Math.round(100 * (userDelta + sysDelta + niceDelta) / totalDelta);
+        }
+
+        // Save values
         this.cpuPrev.user = this.cpu.user;
         this.cpuPrev.sys = this.cpu.sys;
         this.cpuPrev.nice = this.cpu.nice;
         this.cpuPrev.total = this.cpu.total;
-        log(`[TopHat] CPU: ${this.cpuUsage}% on ${this.cpuCores} cores`);
+        for (let i = 0; i < this.cpuCores; i++) {
+            this.cpuPrev.xcpu_user[i] = this.cpu.xcpu_user[i];
+            this.cpuPrev.xcpu_sys[i] = this.cpu.xcpu_sys[i];
+            this.cpuPrev.xcpu_nice[i] = this.cpu.xcpu_nice[i];
+            this.cpuPrev.xcpu_total[i] = this.cpu.xcpu_total[i];
+        }
+
+        // Update UI
+        log(`[TopHat] CPU: ${this.cpuUsage}% on ${this.cpuCores} cores (${this.cpuCoreUsage.join()})`);
         this.valueCPU.text = `${this.cpuUsage}%`;
 
         this.meter.queue_repaint();
@@ -142,10 +174,22 @@ var TopHatCpuIndicator = class TopHatCpuIndicator extends PanelMenu.Button {
         ctx.rectangle(0, 0, width, height);
         ctx.fill();
 
-        Clutter.cairo_set_source_color(ctx, fg);
-        let fillHeight = Math.ceil(this.cpuUsage / 100.0 * height);
-        ctx.rectangle(0, height - fillHeight, width, height);
-        ctx.fill();
+        if (CPU_SHOW_CORES) {
+            Clutter.cairo_set_source_color(ctx, fg);
+            for (let i = 0; i < this.cpuCores; i++) {
+                let barHeight = Math.ceil(this.cpuCoreUsage[i] / 100.0 * height);
+                let x = i * CPU_BAR_WIDTH + i;
+                let y = height - barHeight;
+                ctx.rectangle(x, y, CPU_BAR_WIDTH - 1, barHeight);
+            }
+            ctx.fill();
+        } else {
+            Clutter.cairo_set_source_color(ctx, fg);
+            let fillHeight = Math.ceil(this.cpuUsage / 100.0 * height);
+            ctx.rectangle(0, height - fillHeight, width, height);
+            ctx.fill();
+        }
+        ctx.$dispose();
     }
 
     destroy() {
@@ -224,6 +268,8 @@ var TopHatMemIndicator = class TopHatMemIndicator extends PanelMenu.Button {
         let fillHeight = Math.ceil(this.memUsage / 100.0 * height);
         ctx.rectangle(0, height - fillHeight, width, height);
         ctx.fill();
+
+        ctx.$dispose();
     }
 
     destroy() {
