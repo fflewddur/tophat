4b69b0d79d4dda9e19bedf24d6ba11a7e85510b4
Todd Kulesza <todd@dropline.net>
2022-10-02 17:44:58 -0700

    Include release notes when packaging

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-gnome-extensions pack --force --extra-source lib --extra-source icons --extra-source README.md ./ && echo "Extension successfully packaged"
+gnome-extensions pack --force --extra-source lib --extra-source icons --extra-source README.md RELEASES.md ./ && echo "Extension successfully packaged"
