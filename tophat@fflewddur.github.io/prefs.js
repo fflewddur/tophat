44ec6e0131b1eb9fa6cdcef6d9f10f6138e36b07
Todd Kulesza <todd@dropline.net>
2023-06-03 12:22:15 -0700

    Reword preferences for clarity

--- tophat@fflewddur.github.io/prefs.js
+++ tophat@fflewddur.github.io/prefs.js
@@ -78,22 +78,22 @@ function fillPreferencesWindow(window) {
     group = new Adw.PreferencesGroup({title: _('Disk')});
     addActionRow(_('Show the disk monitor'), 'show-disk', group, configHandler);
     choices = new Gtk.StringList();
+    choices.append(_('Available storage'));
+    choices.append(_('Disk activity'));
+    choices.append(_('Storage and activity'));
+    addComboRow(_('Monitor shows'), choices, 'diskMonitorMode', group, configHandler);
+    choices = new Gtk.StringList();
     choices.append(_('Usage meter'));
     choices.append(_('Numeric value'));
     choices.append(_('Both meter and value'));
-    addComboRow(_('Show as'), choices, 'diskDisplay', group, configHandler);
-    choices = new Gtk.StringList();
-    choices.append(_('Filesystem usage'));
-    choices.append(_('Disk activity'));
-    choices.append(_('Both usage and activity'));
-    addComboRow(_('Monitor'), choices, 'diskMonitorMode', group, configHandler);
+    addComboRow(_('Show available storage as'), choices, 'diskDisplay', group, configHandler);
     choices = new Gtk.StringList();
     let parts = Shared.getPartitions();
     parts.forEach(p => {
         choices.append(p);
     });
     configHandler.setPartitions(choices);
-    addComboRow(_('Disk partition to monitor'), choices, 'mountToMonitor', group, configHandler);
+    addComboRow(_('Filesystem to monitor'), choices, 'mountToMonitor', group, configHandler);
     page.add(group);
 
     group = new Adw.PreferencesGroup({title: _('Network')});
@@ -104,7 +104,7 @@ function fillPreferencesWindow(window) {
     addComboRow(_('Measurement unit'), choices, 'networkUnit', group, configHandler);
     page.add(group);
 
-    window.set_default_size(300, 0);
+    window.set_default_size(400, 0);
 }
 
 function addActionRow(label, setting, group, configHandler) {
