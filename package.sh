0915906a7a01f3339ad80710388aee5664761421
Todd Kulesza <todd@dropline.net>
2023-06-10 14:03:36 -0700

    Include classic stylesheet in package

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-gnome-extensions pack --force --extra-source=lib --extra-source=icons --extra-source=../LICENSE --extra-source=../README.md --extra-source=../RELEASES.md --podir=../po ./tophat@fflewddur.github.io && echo "Extension successfully packaged"
+gnome-extensions pack --force --extra-source=lib --extra-source=icons --extra-source=../LICENSE --extra-source=../README.md --extra-source=../RELEASES.md --extra-source=classic.css --podir=../po ./tophat@fflewddur.github.io && echo "Extension successfully packaged"
