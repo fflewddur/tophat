48b51a160e4bd8d742764b98e9db564c9dce72f5
Todd Kulesza <todd@dropline.net>
2024-10-06 18:56:35 -0700

    Run our standard linter during CI

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-    files: ['**/*.ts'],
+    files: ['src/**/*.ts'],
     extends: [
         eslint.configs.recommended,
         ...tseslint.configs.recommended,
