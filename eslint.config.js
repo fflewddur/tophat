a92c08347b8977324a49803e8ec19c72a2f4f959
Todd Kulesza <todd@dropline.net>
2024-10-07 10:23:55 -0700

    Add the prettier code formatter

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,14 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config({
-    files: ['src/**/*.ts'],
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.recommended,
-    ],
-    rules: {
-
-    }
+  files: ['src/**/*.ts'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+  ],
+  rules: {},
 });
