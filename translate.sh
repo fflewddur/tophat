3f92d5df174c612fac1ce2ff8a991aa597267f6c
Todd Kulesza <todd@dropline.net>
2024-10-28 18:51:52 -0700

    Cleanup project structure

--- translate.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-xgettext --from-code=UTF-8 --package-name=TopHat --copyright-holder="Todd Kulesza" --output=po/tophat.pot tophat@fflewddur.github.io/*.js tophat@fflewddur.github.io/lib/*.js
