3c8f97024f2c9750ee84c0cdd69700aad54bc3e0
Todd Kulesza <todd@dropline.net>
2024-11-18 20:16:37 -0800

    Stub out preferences dialog

--- src/prefs.ts
+++ src/prefs.ts
@@ -30,40 +30,134 @@ export default class TopHatPrefs extends ExtensionPreferences {
     return new Promise<void>((resolve) => {
       this.gsettings = this.getSettings();
 
-      const page = new Adw.PreferencesPage({
-        title: _('General'),
-        iconName: 'dialog-information-symbolic',
-      });
+      window.add(this.buildGeneralPage());
+      window.add(this.buildCpuPage());
+      window.add(this.buildMemPage());
+      window.add(this.buildDiskPage());
+      window.add(this.buildNetPage());
 
-      const group = new Adw.PreferencesGroup({ title: _('General') });
-      page.add(group);
-
-      // Position in panel
-      let choices = new Gtk.StringList();
-      choices.append(_('Left edge'));
-      choices.append(_('Left'));
-      choices.append(_('Center'));
-      choices.append(_('Right'));
-      choices.append(_('Right edge'));
-      this.addComboRow(
-        _('Position in panel'),
-        choices,
-        'position-in-panel',
-        group
-      );
-
-      // Refresh speed
-      choices = new Gtk.StringList();
-      choices.append(_('Slow'));
-      choices.append(_('Medium'));
-      choices.append(_('Fast'));
-      this.addComboRow(_('Refresh speed'), choices, 'refresh-rate', group);
-
-      this.addActionRow(_('Show icons beside monitors'), 'show-icons', group);
-
-      window.add(page);
       resolve();
     });
+  }
+
+  private buildGeneralPage() {
+    const page = new Adw.PreferencesPage({
+      title: _('General'),
+      // iconName: 'dialog-information-symbolic',
+    });
+
+    const group = new Adw.PreferencesGroup({ title: _('General') });
+    page.add(group);
+
+    // Position in panel
+    let choices = new Gtk.StringList();
+    choices.append(_('Left edge'));
+    choices.append(_('Left'));
+    choices.append(_('Center'));
+    choices.append(_('Right'));
+    choices.append(_('Right edge'));
+    this.addComboRow(
+      _('Position in panel'),
+      choices,
+      'position-in-panel',
+      group
+    );
+
+    // Refresh speed
+    choices = new Gtk.StringList();
+    choices.append(_('Slow'));
+    choices.append(_('Medium'));
+    choices.append(_('Fast'));
+    this.addComboRow(_('Refresh speed'), choices, 'refresh-rate', group);
+
+    // Show icons
+    this.addActionRow(_('Show icons beside monitors'), 'show-icons', group);
+
+    return page;
+  }
+
+  private buildCpuPage() {
+    const page = new Adw.PreferencesPage({
+      title: _('CPU'),
+    });
+
+    const group = new Adw.PreferencesGroup({ title: _('CPU') });
+    page.add(group);
+
+    // Enable
+    this.addActionRow(_('Show the CPU monitor'), 'show-cpu', group);
+
+    // Visualization
+    const choices = new Gtk.StringList();
+    choices.append(_('Usage meter'));
+    choices.append(_('Numeric value'));
+    choices.append(_('Both meter and value'));
+    this.addComboRow(_('Show as'), choices, 'cpu-display', group);
+
+    // Show each core
+    this.addActionRow(_('Show each core'), 'cpu-show-cores', group);
+
+    return page;
+  }
+
+  private buildMemPage() {
+    const page = new Adw.PreferencesPage({
+      title: _('Memory'),
+    });
+
+    const group = new Adw.PreferencesGroup({ title: _('Memory') });
+    page.add(group);
+
+    // Enable
+    this.addActionRow(_('Show the memory monitor'), 'show-mem', group);
+
+    // Visualization
+    const choices = new Gtk.StringList();
+    choices.append(_('Usage meter'));
+    choices.append(_('Numeric value'));
+    choices.append(_('Both meter and value'));
+    this.addComboRow(_('Show as'), choices, 'mem-display', group);
+
+    return page;
+  }
+
+  private buildDiskPage() {
+    const page = new Adw.PreferencesPage({
+      title: _('Disk'),
+    });
+
+    const group = new Adw.PreferencesGroup({ title: _('Disk') });
+    page.add(group);
+
+    // Enable
+    this.addActionRow(_('Show the disk monitor'), 'show-disk', group);
+
+    return page;
+  }
+
+  private buildNetPage() {
+    const page = new Adw.PreferencesPage({
+      title: _('Network'),
+    });
+
+    const group = new Adw.PreferencesGroup({ title: _('Network') });
+    page.add(group);
+
+    // Enable
+    this.addActionRow(_('Show the network monitor'), 'show-net', group);
+
+    // Bits or bytes?
+    const choices = new Gtk.StringList();
+    choices.append(_('Bytes'));
+    choices.append(_('Bits'));
+    this.addComboRow(
+      _('Measurement unit'),
+      choices,
+      'network-usage-unit',
+      group
+    );
+
+    return page;
   }
 
   private addActionRow(
