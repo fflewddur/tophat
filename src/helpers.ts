3bce66cf051d63dab3f51f95028244be9457ddbb
Todd Kulesza <todd@dropline.net>
2025-05-26 20:11:34 -0700

    helpers:ts: improve parsing of df output
    - df can exit with an error while still printing
      the filesystem info we need.

--- src/helpers.ts
+++ src/helpers.ts
@@ -140,35 +140,30 @@ export async function readFileSystems(): Promise<FSUsage[]> {
         Gio.SubprocessFlags.STDOUT_PIPE
       );
       proc.communicate_utf8_async(null, null).then(([stdout]) => {
-        if (proc.get_successful()) {
-          const output = stdout as unknown as string;
-          const lines = output.split('\n');
-          for (const line of lines) {
-            const m = line.match(RE_DF_IS_DISK);
-            if (m) {
-              const details = m[2].match(RE_DF_DISK_USAGE);
-              if (details) {
-                const dev = m[1];
-                const cap = parseInt(details[1]) * 1024;
-                const used = parseInt(details[2]) * 1024;
-                const mount = details[5];
-                let fileSystem = new FSUsage(dev, cap, used, mount);
-                if (fileSystems.has(dev)) {
-                  const old = fileSystems.get(dev);
-                  if (old && old.mount.length < mount.length) {
-                    // Only report one mount per device; use the shortest file path
-                    fileSystem = old;
-                  }
+        const output = stdout as unknown as string;
+        const lines = output.split('\n');
+        for (const line of lines) {
+          const m = line.match(RE_DF_IS_DISK);
+          if (m) {
+            const details = m[2].match(RE_DF_DISK_USAGE);
+            if (details) {
+              const dev = m[1];
+              const cap = parseInt(details[1]) * 1024;
+              const used = parseInt(details[2]) * 1024;
+              const mount = details[5];
+              let fileSystem = new FSUsage(dev, cap, used, mount);
+              if (fileSystems.has(dev)) {
+                const old = fileSystems.get(dev);
+                if (old && old.mount.length < mount.length) {
+                  // Only report one mount per device; use the shortest file path
+                  fileSystem = old;
                 }
-                fileSystems.set(dev, fileSystem);
               }
+              fileSystems.set(dev, fileSystem);
             }
           }
+
           resolve(Array.from(fileSystems.values()));
-          return;
-        } else {
-          console.warn('[TopHat] Could not run df -P: ');
-          reject('Could not run df -P');
           return;
         }
       });
